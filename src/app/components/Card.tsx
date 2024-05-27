98dece1cbd992010e4b5eef8fda12985939a8bb4
AkiraAcetonoshi <facilitateurmaker@gmail.com>
2024-05-27 18:08:17 +0200

    modified the behaviour of cards backlighting and removed the unnecessary button wrapping the anchor in the cards

--- src/app/components/Card.tsx
+++ src/app/components/Card.tsx
@@ -1,13 +1,12 @@
-type Project =  {
-    "name": string;
-    "url": string;
-    "repo_url": string;
-    "description": string;
-    "tags":string[];
-  };
+type Project = {
+  name: string;
+  url: string;
+  repo_url: string;
+  description: string;
+  tags: string[];
+};
 
-export default function Card( { project }: { project: Project}) {
-
+export default function Card({ project }: { project: Project }) {
   return (
     <article className="card">
       <section className="project-info-section">
@@ -20,22 +19,20 @@ export default function Card( { project }: { project: Project}) {
 
         <section className="tags-section">
           {project.tags.map((tag) => (
-            <p key={`${project.name}-${tag}`}className="tag">{tag}</p>
+            <p key={`${project.name}-${tag}`} className="tag">
+              {tag}
+            </p>
           ))}
         </section>
       </section>
 
       <section className="buttons-section">
-        <button role="button" className="button-to-website">
-          <a href={project.url} target="_blank">
-            Voir le site
-          </a>
-        </button>
-        <button role="button" className="button-to-github">
-          <a href={project.repo_url} target="_blank">
-            Voir le code
-          </a>
-        </button>
+        <a className="link-to-website" href={project.url} target="_blank">
+          Voir le site
+        </a>
+        <a className="link-to-github" href={project.repo_url} target="_blank">
+          Voir le code
+        </a>
       </section>
     </article>
   );
