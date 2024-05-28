1420aae21e4554798f996cde1a48abe3e24feaae
AkiraAcetonoshi <facilitateurmaker@gmail.com>
2024-05-28 10:27:48 +0200

    animated title and fixed keyboard navigation quirk on cards buttons

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -2,12 +2,16 @@ export default function Header() {
   return (
     <header>
       <hgroup>
-        <h1>Hi, i&apos;m David. :D</h1>
-        <p>
+        <h1>
+          <span className="title-hi">Hi, </span>{" "}
+          <span className="title-name">i&apos;m David. </span>{" "}
+          <span className="title-smiley"> :D</span>
+        </h1>
+        <h3>
           Développeur web fort d&apos;une expérience en ingénierie, design
           industriel et management, je suis prêt à apporter ma curiosité et mes
           compétences à des projets nouveaux.
-        </p>
+        </h3>
       </hgroup>
     </header>
   );
