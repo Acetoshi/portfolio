628dc9b16e4526ba370b7a56eb4edaecea3ceb19
AkiraAcetonoshi <facilitateurmaker@gmail.com>
2024-05-29 09:20:23 +0200

    added a state and a media query for the mobile menu

--- src/app/components/NavBar.tsx
+++ src/app/components/NavBar.tsx
@@ -1,14 +1,24 @@
 "use client";
-
+import { useState } from "react";
 export default function NavBar() {
-    
+  const [isCollapsed, setIsCollapsed] = useState(true);
+
   function ScrollToSection(sectionClassName: string) {
     const section = document.getElementsByClassName(sectionClassName)[0];
     section.scrollIntoView({ behavior: "smooth" });
   }
 
+  function toggleMenu() {
+    setIsCollapsed(() => !isCollapsed);
+  }
+
   return (
-    <nav>
+    <nav className={isCollapsed ? "collapsed" : ""}>
+      <button role="button" className="nav-menu-button" onClick={toggleMenu}>
+        <div></div>
+        <div></div>
+        <div></div>
+      </button>
       <ul>
         <li>
           <button role="button" onClick={() => ScrollToSection("projects")}>
