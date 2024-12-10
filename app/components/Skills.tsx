971d9d89516de5e6ca422f97489f5de7c94fb91b
AkiraAcetonoshi <facilitateurmaker@gmail.com>
2024-12-10 19:40:17 +0100

    CHORE : updated skills list

--- app/components/Skills.tsx
+++ app/components/Skills.tsx
@@ -7,26 +7,39 @@ export default function Skills() {
           <h3>RELATIONNELLES</h3>
           <ul>
             <li>Adaptabilité & Flexibilité</li>
-            <li>Forte curiosité et Goût pour l&apos;apprentissage</li>
-            <li>Communication assertive & <br /> Remise en question</li>
-            <li>Résolution méthodique de problèmes, Esprit analytique,<br /> Autonomie dans la recherche de solutions</li>
-            <li>Anglais courant TOEIC 945</li>
+            <li>Curiosité et Goût pour l'apprentissage</li>
+            <li>Communication assertive</li>
+            <li>Résolution de problèmes, Esprit analytique</li>
             <li>Recrutement, Management, Formation</li>
+            <li>Prospection et vente</li>
+            <li>Méthodes agiles SCRUM</li>
+            <li>Travail en équipe & Collaboration</li>
+            <li>Gestion du stress & Priorisation</li>
+            <li>Prise de décision</li>
+            <li>Gestion de projet</li>
+            <li>Anglais courant TOEIC 945</li>
           </ul>
         </section>
         <section className="hard-skills">
           <h3> TECHNIQUES</h3>
           <ul>
-            <li>HTML, CSS, JavaScript, TypeScript, Python, C++ embarqué</li>
-            <li>MySQL, Supabase</li>
-            <li>Affordance & Accessibilité (A11Y)</li>
-            <li>ReactJS, Express.js</li>
+            <li>HTML, CSS, JavaScript, TypeScript, Python, C++</li>
+            <li>ReactJS, Next.js, Express.js, Angular</li>
+
+            <li>
+              Material UI, LeafletJS, Monaco Editor, GraphQL, JWT, Web Workers
+            </li>
+            <li>MySQL, SQLite, PostGreSQL, Supabase</li>
+
             <li>Git, Github, Figma (UX/UI)</li>
-            <li>Intégration continue (CI/CD)</li>
+            <li>
+              CI/CD : Intégration continue, Jest, Vitest, Playwright, Docker
+            </li>
+            <li>
+              Autres : Microsoft PowerApps, Programmation VBA, Python, C++
+            </li>
+            <li>Environnements : Windows, Arch Linux, Debian</li>
             <li>Veille technique variée</li>
-            <li>Microsoft PowerApps, VBA</li>
-            <li>Conception mécanique 3D</li>
-            <li>Windows, Arch Linux, Debian</li>
           </ul>
         </section>
       </section>
